1fc120050af8df1f3aa0a0196e1eca149007422b
Nicolas A. Dela Cruz <ndelacruz@cascade.local>
2022-06-16 15:03:51 -0700

    code from stackoverflow is op

--- js/scripts.js
+++ js/scripts.js
@@ -24,45 +24,115 @@ var dateData = today;
 // [currentUsername, currentPriority, itemLink, quantity, today];
 
 function clickMe(){
-    console.log('hi');
+    console.log('submitted');
 }
 
-document.getElementById("done").addEventListener("click", myFunction);
+document.getElementById("done").addEventListener("click", clickMe);
 
-function myFunction() {
+// function myFunction() {
     
-    document.getElementById("date_display").innerHTML = dateData;
-    document.getElementById("user_display").innerHTML = submitUsername.value;
-    document.getElementById("priority_display").innerHTML = submitPriority.value;
-    document.getElementById("link_display").innerHTML = itemDisplay.value;
-    document.getElementById("howMany_display").innerHTML = howManyDisplay.value;
-}
-
-
-var updateUsername = document.getElementById('username_list').addEventListener('click', function(e) {
-    document.getElementById("user_display").innerHTML = (e.target.options[e.target.selectedIndex].getAttribute('id'));
-
-
-    // console.log(e.target.options[e.target.selectedIndex].getAttribute('id'));
-});
-
-<<<<<<< HEAD
-
-
-=======
-var updatePrio = document.getElementById('priority_list').addEventListener('change', function(e) {
-    console.log(e.target.options[e.target.selectedIndex].getAttribute('id'));
-});
-
-var updateLink = document.getElementById('howMany').addEventListener('change', function(e) {
-    console.log(e.target[e.target.selectedIndex].getAttribute('value'));
-});
-
-
-// function val() {
-//     d = document.getElementById("howMany").value;
-//     alert(d);
+//     document.getElementById("date_display").innerHTML = dateData;
+//     document.getElementById("user_display").innerHTML = submitUsername.value;
+//     document.getElementById("priority_display").innerHTML = submitPriority.value;
+//     document.getElementById("link_display").innerHTML = itemDisplay.value;
+//     document.getElementById("howMany_display").innerHTML = howManyDisplay.value;
 // }
 
->>>>>>> e773ce3f95eb81ea2c406e42c9baea2f3132f304
+// let formDiv = ['username_list',]
+
+var updateUsername = document.getElementById('username_list').addEventListener('change', function(e) {
+    document.getElementById("user_display").innerHTML = (e.target.options[e.target.selectedIndex].getAttribute('id'));
+    
+});
+
+var updatePriority = document.getElementById('priority_list').addEventListener('change', function(e) {
+    document.getElementById("priority_display").innerHTML = (e.target.options[e.target.selectedIndex].getAttribute('id'));
+    
+});
+
+
+var updateDate = document.getElementById("howMany").addEventListener('change', function(e) {
+    document.getElementById("howMany_display").innerHTML = document.getElementById('howMany').value;
+    
+});
+
+
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+input.addEventListener('change', updateValue);
+function updateValue(e) {
+  log.textContent = e.target.value;
+  const url = link.value;
+  window.open(url);
+}
+
+// const link = document.getElementById("link");
+// const button = document.getElementById("done");
+// button.addEventListener("click", event => {
+//   const url = link.value;
+//   window.open(url);
+// });
+
+
+var urlList = [];
+
+function getUrlList() {
+
+  var url = {
+    urlhtml
+  };
+  var i = 0;
+  var thisList = "";
+
+  url.urlhtml = document.getElementById("urlhtml").value;
+
+  urlList.push(url);
+  for (i = 0; i < urlList.length; i++) {
+    thisList += "<a target='blank' href='http://" + urlList[i].urlhtml + "'>" + urlList[i].urlhtml + "</a><br>";
+  }
+  document.getElementById("showurls").innerHTML = thisList;
+}
+
+// var rosterListings = "";
+//     for (var i = 0; i < teamRoster.length; i++) {
+//         rosterListings = rosterListings + " <a href='" + idList[i] + "'>" + teamRoster[i] + "</a>,";
+//     }
+//     $("#teamRoster").text(rosterListings);
+
+
+
+// document.getElementById("goto").addEventListener("click", goTo);
+
+// let inputVal = document.querySelector('#link').value;
+// //Manipulate your URL, add https:// or replace http:// with https:// or add www:
+
+
+
+// window.open(inputVal);
+
+// function goTo() {  
+//   var result = document.getElementById("searchbar").value;  
+//   window.location.href = '/user/'+result;
+// }
+
+// const input = document.querySelector('link');
+// const log = document.getElementById('link_display');
+
+// input.addEventListener('change', updateValue);
+
+// function updateValue(e) {
+//   log.textContent = e.target.value;
+// }
+
+
+// var updateLink = document.getElementById("link").addEventListener('change', function(e) {
+//     document.getElementById("link_display").innerHTML = document.getElementById('link').value;
+    
+// });
+
+
+
+
+
 console.log('hello, you reached the end of the Script');
