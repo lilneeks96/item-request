83212b44d71f0faf87485e0ab4d04fa3fd1a7107
Nicolas A. Dela Cruz <ndelacruz@cascade.local>
2022-06-21 11:35:04 -0700

    html tut

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,3 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("recipe1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+let el = document.getElementById("#text")
+
+
 
 
 
